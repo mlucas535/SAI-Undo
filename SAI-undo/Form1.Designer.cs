24cd6a2ef3ba1e943090a2b39a7626700dac77c7
Mike <Mike@DESKTOP-KLF9J2A>
2017-09-11 18:56:29 -0500

    Add project files.

---
+++ SAI-undo/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace SAI_undo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.versionTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.currentUndoTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.newUndoTextBox = new System.Windows.Forms.TextBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(58, 278);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(171, 78);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Open";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // versionTextBox
+            // 
+            this.versionTextBox.Location = new System.Drawing.Point(58, 94);
+            this.versionTextBox.Name = "versionTextBox";
+            this.versionTextBox.ReadOnly = true;
+            this.versionTextBox.Size = new System.Drawing.Size(144, 31);
+            this.versionTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SAI Version:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current Max Undos:";
+            // 
+            // currentUndoTextBox
+            // 
+            this.currentUndoTextBox.Location = new System.Drawing.Point(58, 205);
+            this.currentUndoTextBox.Name = "currentUndoTextBox";
+            this.currentUndoTextBox.ReadOnly = true;
+            this.currentUndoTextBox.Size = new System.Drawing.Size(100, 31);
+            this.currentUndoTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(369, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(235, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "New Amount Of Undos:";
+            // 
+            // newUndoTextBox
+            // 
+            this.newUndoTextBox.Location = new System.Drawing.Point(374, 94);
+            this.newUndoTextBox.Name = "newUndoTextBox";
+            this.newUndoTextBox.Size = new System.Drawing.Size(100, 31);
+            this.newUndoTextBox.TabIndex = 6;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(374, 278);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(163, 78);
+            this.updateButton.TabIndex = 7;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 387);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(448, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Program written by @Best_Dude55 on Twitter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 421);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.newUndoTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.currentUndoTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.versionTextBox);
+            this.Controls.Add(this.openFileButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SAI Undo Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.TextBox versionTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox currentUndoTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox newUndoTextBox;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SAI-undo/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SAI_undo
+{
+    public partial class Form1 : Form
+    {
+        private string filePath;
+        private string versionNumber;
+        private int offset;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selectSAIDialog = new OpenFileDialog();
+            
+
+            selectSAIDialog.ShowDialog();
+            filePath = selectSAIDialog.FileName;
+            //textBox1.Text = filePath;
+
+            versionNumber = findVersionNumber();
+            versionTextBox.Text = versionNumber;
+            offset = findUndoOffset();
+        }
+
+        private bool compareVersionText(byte[] goalArray, byte[] readData)
+        {
+            for(int i = 0; i < 14; i++)
+            {
+                if(goalArray[i] != readData[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private string findVersionNumber() //Fix loop to not run infinitely in case incorrect file is loaded
+        {
+            byte[] versionSeries = { 0x61, 0x69, 0x20, 0x56, 0x65, 0x72, 0x73, 0x69, 0x6F, 0x6E, 0x20, 0x20, 0x3D, 0x20 };
+            string verNumber = "";
+
+            try
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+                {
+                    byte beginLetter;
+                    int offset = 0;
+                    do
+                    {
+                        try { beginLetter = reader.ReadByte(); }
+                        catch (EndOfStreamException e)
+                        {
+                            MessageBox.Show("Error loading SAI!\nDid you make sure to load the .exe file and not the shortcut?", "Error");
+                            return "Error";
+                        }
+
+                        if (beginLetter == 0x53)
+                        {
+                            byte[] versionCompare = { };
+                            try
+                            {
+                                versionCompare = reader.ReadBytes(14);
+                            }
+                            catch (EndOfStreamException e)
+                            {
+                                MessageBox.Show("Error loading SAI!\nDid you make sure to load the .exe file and not the shortcut?", "Error");
+                                return "Error";
+                            }
+
+                            if (compareVersionText(versionSeries, versionCompare))
+                            {
+                                byte[] versionNumberArray = reader.ReadBytes(5);
+                                verNumber = System.Text.Encoding.UTF8.GetString(versionNumberArray);
+                                versionTextBox.Text = verNumber;
+                                return verNumber;
+                            }
+                        }
+                        offset++;
+                    } while (true);
+                }
+            }
+            catch(UnauthorizedAccessException e)
+            {
+                MessageBox.Show("Error accessing file.\nYou may have to re-run this program as an administrator.", "Error");
+                return "Error";
+            }
+        }
+
+        private int findUndoOffset()
+        {
+            byte[] undoInstruction = new byte[4];
+
+            if (versionNumber.Equals("1.1.0")) {
+                undoInstruction[0] = 0x74;
+                undoInstruction[1] = 0x3C;
+                undoInstruction[2] = 0xBB;
+            }else
+            {
+                undoInstruction[0] = 0xFF;
+                undoInstruction[1] = 0x40;
+                undoInstruction[2] = 0x08;
+                undoInstruction[3] = 0xBF;
+            }
+
+            try
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+                {
+                    byte beginByte;
+                    int offset = 0;
+                    do
+                    {
+                        try
+                        {
+                            beginByte = reader.ReadByte();
+                        }
+                        catch (EndOfStreamException e)
+                        {
+                            currentUndoTextBox.Text = "Error";
+                            return -1;
+                        }
+
+                        if (beginByte == undoInstruction[0])
+                        {
+                            byte[] instructionCompare = { };
+                            try
+                            {
+                                if (versionNumber.Equals("1.1.0"))
+                                {
+                                    instructionCompare = reader.ReadBytes(2);
+                                }
+                                else
+                                {
+                                    instructionCompare = reader.ReadBytes(3);
+                                }
+                                
+                            }
+                            catch (EndOfStreamException e)
+                            {
+                                currentUndoTextBox.Text = "Error";
+                                return -1;
+                            }
+
+                            if (versionNumber.Equals("1.1.0"))
+                            {
+                                offset += 2;
+                            }
+                            else
+                            {
+                                offset += 3;
+                            }
+
+                            if (compareInstructions(undoInstruction, instructionCompare))
+                            {
+                                offset++;
+                                string sampleString = reader.ReadInt32().ToString();
+                                currentUndoTextBox.Text = sampleString;
+                                return offset;
+                            }
+                        }
+                        offset++;
+                    } while (true);
+                }
+            }
+            catch(UnauthorizedAccessException e)
+            {
+                currentUndoTextBox.Text = "Error";
+                return -1;
+            }
+        }
+
+        private bool compareInstructions(byte[] goalInstruction, byte[] readInstruction)
+        {
+            int length;
+            if (versionNumber.Equals("1.1.0"))
+            {
+                length = 2;
+            }
+            else
+            {
+                length = 3;
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                if (goalInstruction[i + 1] != readInstruction[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void displayCurrentUndoSetting()
+        {
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            if (versionTextBox.Text.Equals("Error"))
+            {
+                MessageBox.Show("Please load a valid copy of SAI before continuing.", "Error");
+            }else if(versionTextBox.Text.Equals(""))
+            {
+                MessageBox.Show("Please load SAI using the 'Open' button before continuing.", "Error");
+            }
+            else
+            {
+                int newUndoNumber = 0;
+                try
+                {
+                    newUndoNumber = Int32.Parse(newUndoTextBox.Text);
+                }catch(FormatException ex)
+                {
+                    MessageBox.Show("Please enter a valid number", "Error");
+                    return;
+                }
+
+                if(newUndoNumber >= 1 && newUndoNumber <= 5000)
+                    updateUndos(offset);
+                else
+                    MessageBox.Show("Please input a number between 1 and 5000", "Error");
+            }
+        }
+
+        private void updateUndos(int offset)
+        {
+            try
+            {
+                using (BinaryWriter writer = new BinaryWriter(new FileStream(filePath, FileMode.Open, FileAccess.Write)))
+                {
+                    int updatedUndos = Int32.Parse(newUndoTextBox.Text);
+                    writer.Seek(offset, 0);
+                    writer.Write(updatedUndos);
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Error writing to file.", "Error");
+            }
+
+            findUndoOffset();
+        }
+    }
+}
